61ba853dd9a68ac3780c65042ed4c0c7e96b1980
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-10-24 14:54:20 +0100

    FirstSteps in UI

---
+++ UltimatePortfolio/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Gary Watson on 24/10/2020.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
