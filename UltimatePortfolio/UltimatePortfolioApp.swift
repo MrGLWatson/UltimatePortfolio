e9547ecca897c3cf97c161f4e0b5b22b741d5f4a
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-10-24 10:56:01 +0100

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Gary Watson on 24/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
