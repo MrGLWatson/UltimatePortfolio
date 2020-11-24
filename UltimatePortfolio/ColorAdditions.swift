885ba37c1d8bdf9c89b3dcc041a68e693c464045
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-11-24 13:05:47 +0000

    Creating the home view

---
+++ UltimatePortfolio/ColorAdditions.swift
@@ -0,0 +1,13 @@
+//
+//  ColorAdditions.swift
+//  UltimatePortfolio
+//
+//  Created by Gary Watson on 24/11/2020.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
