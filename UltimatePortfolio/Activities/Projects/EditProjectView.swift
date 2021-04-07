6b9efe461a76c6c25cfe6d92832f752b5265fae1
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2021-04-07 14:38:41 +0100

    Adding local notifications

--- UltimatePortfolio/Activities/Projects/EditProjectView.swift
+++ UltimatePortfolio/Activities/Projects/EditProjectView.swift
@@ -16,12 +16,25 @@ struct EditProjectView: View {
     @State private var detail: String
     @State private var color: String
     @State private var showingDeleteConfirm = false
+    @State private var showingNotificationsError = false
+
+    @State private var remindMe: Bool
+    @State private var reminderTime: Date
+
     @State private var engine = try? CHHapticEngine()
     init(project: Project) {
         self.project = project
         _title = State(wrappedValue: project.projectTitle)
         _detail = State(wrappedValue: project.projectDetail)
         _color = State(wrappedValue: project.projectColor)
+
+        if let projectReminderTime = project.reminderTime {
+            _reminderTime = State(wrappedValue: projectReminderTime)
+            _remindMe = State(wrappedValue: true)
+        } else {
+            _reminderTime = State(wrappedValue: Date())
+            _remindMe = State(wrappedValue: false)
+        }
     }
     let colorColumns = [
         GridItem(.adaptive(minimum: 44))
@@ -40,7 +53,24 @@ struct EditProjectView: View {
                 }
                 .padding(.vertical)
             }
-            // section 3
+
+            Section(header: Text("Project reminders")) {
+                Toggle("Show reminders", isOn: $remindMe.animation().onChange(update))
+                    .alert(isPresented: $showingNotificationsError) {
+                        Alert(title: Text("Oops"),
+                              message: Text("Thee was a problem, please check you have notifications enabled."),
+                              primaryButton: .default(Text("Check Settings"), action: showAppSettings),
+                              secondaryButton: .cancel())
+                    }
+
+                if remindMe {
+                    DatePicker(
+                        "Reminder time",
+                        selection: $reminderTime.onChange(update),
+                        displayedComponents: .hourAndMinute
+                    )
+                }
+            }
             // swiftlint:disable:next line_length
             Section(footer: Text("Closing a project moves it from the Open to Closed tab; deleting it removes the project entirely")) {
                 Button(project.closed ? "Reopen this project" : "Close this project", action: toggleClosed)
@@ -64,6 +94,22 @@ struct EditProjectView: View {
         project.title = title
         project.detail = detail
         project.color = color
+
+        if remindMe {
+            project.reminderTime = reminderTime
+
+            dataController.addReminders(for: project) { success in
+                if success == false {
+                    project.reminderTime = nil
+                    remindMe = false
+
+                    showingNotificationsError = true
+                }
+            }
+        } else {
+            project.reminderTime = nil
+            dataController.removeReminders(for: project)
+        }
     }
     func delete() {
         dataController.delete(project)
@@ -128,6 +174,16 @@ struct EditProjectView: View {
                 : .isButton // we are a button and if color matches we are also selected
         )
         .accessibilityLabel(LocalizedStringKey(item))
+    }
+
+    func showAppSettings() {
+        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+
+        if UIApplication.shared.canOpenURL(settingsURL) {
+            UIApplication.shared.open(settingsURL)
+        }
     }
 }
 
